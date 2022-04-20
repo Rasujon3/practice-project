4cbc896232c35fadb635bb4f29bc607e293a3991
Rasujon3 <rasujon3@gmail.com>
2022-04-21 03:02:49 +0600

    jwt token add

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -1,4 +1,5 @@
 const ProfileModel = require("../models/ProfileModel");
+var jwt = require('jsonwebtoken');
 
 exports.CreateProfile = (req, res) => {
   let reqBody = req.body;
@@ -12,8 +13,24 @@ exports.CreateProfile = (req, res) => {
 };
 
 exports.UserLogin = (req, res) => {
-  let reqBody = req.body;
   let UserName = req.body['UserName'];
   let Password = req.body['Password'];
-  res.status(200).json({ status: "success", data: Password });
+
+  ProfileModel.find({UserName:UserName,Password:Password}, (err,data)=>{
+    if (err){
+      res.status(400).json({ status: "fail", data: err });
+    } else {
+      if (data.length>0){
+        // Create Auth Token
+        let Payload = {exp: Math.floor(Date.now() / 1000) + (24*60 * 60), data: data[0]}
+        let token = jwt.sign(Payload, 'SecretKey123456789');
+
+        res.status(200).json({ status: "success",token:token, data: data });
+      } else  {
+        res.status(401).json({ status: "unauthorized" });
+      }
+    }
+
+  })
+
 };
