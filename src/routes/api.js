2dc960211562ca39a32691720ed19e9fa438792f
Rasujon3 <rasujon3@gmail.com>
2022-04-20 19:28:16 +0600

    user registration part 1

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const ProfileController = require("../controllers/ProfileController");
 const router = express.Router();
 
 // route add
+router.post("/CreateProfile", ProfileController.CreateProfile);
 module.exports = router;
