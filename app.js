757c650173cb726d286fd43d0d9edfdfc36b15b9
Rasujon3 <rasujon3@gmail.com>
2022-04-20 19:50:34 +0600

    user registration part 2 - username make unique

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
 // Basic Lib Import
-const express = require('express');
-const router = require('./src/routes/api');
+const express = require("express");
+const router = require("./src/routes/api");
 const app = new express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 // Security Middleware Lib Import
-const rateLimit = require('express-rate-limit');
-const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
-const hpp = require('hpp');
-const cors = require('cors');
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const cors = require("cors");
 
 // Database Lib Import
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Security Middleware Import
-app.use(cors())
-app.use(helmet())
-app.use(mongoSanitize())
-app.use(xss())
-app.use(hpp())
+app.use(cors());
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
 
 //Body Parser Implement
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // Request Rate Limit
-const limiter = rateLimit({windowMs: 15*60*1000, max: 3000})
-app.use(limiter)
+const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 3000 });
+app.use(limiter);
 
 // MongoDB Database Connection
 let URI = "mongodb://localhost:27017/Todo";
 // let URI = "mongodb://127.0.0.1:27017/Todo"
-let OPTION={user:'',pass:''}
-mongoose.connect(URI,OPTION,(error)=>{
-    console.log("Connection Success")
-    console.log(error)
-})
+let OPTION = { user: "", pass: "", autoIndex: true };
+mongoose.connect(URI, OPTION, (error) => {
+  console.log("Connection Success");
+  console.log(error);
+});
 
 // Routing Implement
-app.use("/api/v1/",router)
+app.use("/api/v1/", router);
 
 // Undefined Routing Implement
-app.use("*",(req,res)=>{
-    res.status(404).json({status: "fail",data:"Not Found"})
-})
+app.use("*", (req, res) => {
+  res.status(404).json({ status: "fail", data: "Not Found" });
+});
 
 module.exports = app;
