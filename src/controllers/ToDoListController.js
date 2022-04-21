6cd8259e6678f602a27f5d9e52d6b9b8f827ab8f
Rasujon3 <rasujon3@gmail.com>
2022-04-21 13:23:04 +0600

    todo list create done

---
+++ src/controllers/ToDoListController.js
@@ -0,0 +1,30 @@
+const TodoListModel = require("../models/TodoListModel");
+const jwt = require('jsonwebtoken');
+
+exports.CreateToDo = (req, res) => {
+    let reqBody = req.body;
+
+    let TodoSubject = reqBody['TodoSubject'];
+    let TodoDescription = reqBody['TodoDescription'];
+    let UserName = req.headers['username']
+    let TodoStatus = "New";
+    let TodoCreateDate = Date.now();
+    let TodoUpdateDate = Date.now();
+
+    let PostBody = {
+        UserName: UserName,
+        TodoSubject: TodoSubject,
+        TodoDescription: TodoDescription,
+        TodoStatus: TodoStatus,
+        TodoCreateDate: TodoCreateDate,
+        TodoUpdateDate:TodoUpdateDate
+    }
+
+    TodoListModel.create(PostBody, (err, data) => {
+        if (err) {
+            res.status(400).json({ status: "fail", data: err });
+        } else {
+            res.status(200).json({ status: "success", data: data });
+        }
+    });
+};
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const ProfileController = require("../controllers/ProfileController");
+const ToDoListController = require("../controllers/ToDoListController");
 const AuthVerifyMiddleware = require('../middleware/AuthVerifyMiddleware');
 
 
@@ -11,5 +12,8 @@ router.post("/UserLogin", ProfileController.UserLogin);
 
 router.get("/SelectProfile",AuthVerifyMiddleware, ProfileController.SelectProfile);
 router.post("/UpdateProfile",AuthVerifyMiddleware, ProfileController.UpdateProfile);
+
+router.post("/CreateToDo",AuthVerifyMiddleware, ToDoListController.CreateToDo);
+
 
 module.exports = router;
