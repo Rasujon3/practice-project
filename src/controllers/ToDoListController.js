93b45c023b43c41a2debd8cc1d93c506a58ea2b4
Rasujon3 <rasujon3@gmail.com>
2022-04-21 16:23:24 +0600

    all done

--- src/controllers/ToDoListController.js
+++ src/controllers/ToDoListController.js
@@ -1,93 +1,130 @@
-const TodoListModel = require("../models/TodoListModel");
-const jwt = require('jsonwebtoken');
+const ToDoListModel = require("../models/ToDoListModel");
 
-exports.CreateToDo = (req, res) => {
-    let reqBody = req.body;
 
-    let TodoSubject = reqBody['TodoSubject'];
-    let TodoDescription = reqBody['TodoDescription'];
-    let UserName = req.headers['username']
-    let TodoStatus = "New";
-    let TodoCreateDate = Date.now();
-    let TodoUpdateDate = Date.now();
+exports.CreateToDo=(req,res)=>{
+    let reqBody=req.body;
+    let TodoSubject=reqBody['TodoSubject']
+    let TodoDescription=reqBody['TodoDescription']
+    let UserName=req.headers['username']
+    let TodoStatus="New"
+    let TodoCreateDate=Date.now();
+    let TodoUpdateDate=Date.now();
 
-    let PostBody = {
-        UserName: UserName,
-        TodoSubject: TodoSubject,
-        TodoDescription: TodoDescription,
-        TodoStatus: TodoStatus,
-        TodoCreateDate: TodoCreateDate,
+    let PostBody={
+        UserName:UserName,
+        TodoSubject:TodoSubject,
+        TodoDescription:TodoDescription,
+        TodoStatus:TodoStatus,
+        TodoCreateDate:TodoCreateDate,
         TodoUpdateDate:TodoUpdateDate
     }
 
-TodoListModel.create(PostBody, (err, data) => {
-    if (err) {
-        res.status(400).json({ status: "fail", data: err });
-    } else {
-        res.status(200).json({ status: "success", data: data });
-    }
-});
-};
-
-exports.SelectToDo = (req, res) => {
-    let UserName = req.headers['username'];
-    TodoListModel.find({UserName:UserName},(err,data)=>{
-
-        if (err) {
-            res.status(400).json({ status: "fail", data: err });
-        } else {
-            res.status(200).json({ status: "success", data: data });
+    ToDoListModel.create(PostBody,(err,data)=>{
+        if(err){
+            res.status(400).json({status:"fail",data:err})
+        }
+        else {
+            res.status(200).json({status:"success",data:data})
         }
     })
-};
+}
 
-exports.UpdateToDo = (req, res) => {
-    let TodoSubject = req.body['TodoSubject']
-    let TodoDescription= req.body['TodoDescription']
-    let _id = req.body['_id']
-    let TodoUpdateDate = Date.now();
+exports.SelectToDo=(req,res)=>{
+    let UserName=req.headers['username']
+    ToDoListModel.find({UserName:UserName},(err,data)=>{
+        if(err){
+            res.status(400).json({status:"fail",data:err})
+        }
+        else {
+            res.status(200).json({status:"success",data:data})
+        }
+    })
+}
 
-    let PostBody = {
+exports.UpdateToDo=(req,res)=>{
+
+    let TodoSubject=req.body['TodoSubject']
+    let TodoDescription=  req.body['TodoDescription']
+    let _id=  req.body['_id']
+    let TodoUpdateDate=Date.now();
+
+    let PostBody={
         TodoSubject:TodoSubject,
         TodoDescription:TodoDescription,
         TodoUpdateDate:TodoUpdateDate,
     }
 
-    TodoListModel.updateOne({_id:_id},{$set:PostBody},{upsert:true},(err,data)=>{
-        if (err) {
-            res.status(400).json({ status: "fail", data: err });
-        } else {
-            res.status(200).json({ status: "success", data: data });
+    ToDoListModel.updateOne({_id:_id},{$set:PostBody},{upsert:true},(err,data)=>{
+        if(err){
+            res.status(400).json({status:"fail",data:err})
+        }
+        else {
+            res.status(200).json({status:"success",data:data})
         }
     })
 
-};
+}
 
-exports.UpdateStatusToDo = (req, res) => {
-    let TodoStatus = req.body['TodoStatus'];
-    let _id = req.body['_id']
-    let TodoUpdateDate = Date.now();
 
-    let PostBody = {
+exports.UpdateStatusToDo=(req,res)=>{
+
+    let TodoStatus=req.body['TodoStatus']
+    let _id=  req.body['_id']
+    let TodoUpdateDate=Date.now();
+
+    let PostBody={
         TodoStatus:TodoStatus,
         TodoUpdateDate:TodoUpdateDate,
     }
 
-    TodoListModel.updateOne({_id:_id},{$set:PostBody},{upsert:true},(err,data)=>{
-        if (err) {
-            res.status(400).json({ status: "fail", data: err });
-        } else {
-            res.status(200).json({ status: "success", data: data });
+    ToDoListModel.updateOne({_id:_id},{$set:PostBody},{upsert:true},(err,data)=>{
+        if(err){
+            res.status(400).json({status:"fail",data:err})
+        }
+        else {
+            res.status(200).json({status:"success",data:data})
         }
     })
 
-};
+}
 
 exports.RemoveToDo=(req,res)=>{
 
     let _id=  req.body['_id']
 
-    TodoListModel.remove({_id:_id},(err,data)=>{
+    ToDoListModel.remove({_id:_id},(err,data)=>{
+        if(err){
+            res.status(400).json({status:"fail",data:err})
+        }
+        else {
+            res.status(200).json({status:"success",data:data})
+        }
+    })
+}
+
+
+exports.SelectToDoByStatus=(req,res)=>{
+    let UserName=req.headers['username']
+    let TodoStatus=  req.body['TodoStatus']
+    ToDoListModel.find({UserName:UserName,TodoStatus:TodoStatus},(err,data)=>{
+        if(err){
+            res.status(400).json({status:"fail",data:err})
+        }
+        else {
+            res.status(200).json({status:"success",data:data})
+        }
+    })
+}
+
+
+
+
+exports.SelectToDoByDate=(req,res)=>{
+    let UserName=req.headers['username']
+    let FormDate=  req.body['FormDate']
+    let ToDate=  req.body['ToDate']
+
+    ToDoListModel.find({UserName:UserName,TodoCreateDate:{$gte:new Date(FormDate),$lte:new Date(ToDate)}},(err,data)=>{
         if(err){
             res.status(400).json({status:"fail",data:err})
         }
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,23 +1,28 @@
-const express = require("express");
-const ProfileController = require("../controllers/ProfileController");
-const ToDoListController = require("../controllers/ToDoListController");
-const AuthVerifyMiddleware = require('../middleware/AuthVerifyMiddleware');
+const express =require('express');
+const ProfileController=require("../controllers/ProfileController")
+const ToDoListController=require("../controllers/ToDoListController")
+const AuthVerifyMiddleware=require("../middleware/AuthVerifyMiddleware");
 
 
-const router = express.Router();
+const router =express.Router();
 
-// route add
-router.post("/CreateProfile", ProfileController.CreateProfile);
-router.post("/UserLogin", ProfileController.UserLogin);
 
-router.get("/SelectProfile",AuthVerifyMiddleware, ProfileController.SelectProfile);
-router.post("/UpdateProfile",AuthVerifyMiddleware, ProfileController.UpdateProfile);
+router.post("/CreateProfile",ProfileController.CreateProfile)
+router.post("/UserLogin",ProfileController.UserLogin)
 
-router.post("/CreateToDo",AuthVerifyMiddleware, ToDoListController.CreateToDo);
-router.get("/SelectToDo",AuthVerifyMiddleware, ToDoListController.SelectToDo);
-router.post("/UpdateToDo",AuthVerifyMiddleware, ToDoListController.UpdateToDo);
-router.post("/UpdateStatusToDo",AuthVerifyMiddleware, ToDoListController.UpdateStatusToDo);
+
+
+router.get("/SelectProfile",AuthVerifyMiddleware,ProfileController.SelectProfile);
+router.post("/UpdateProfile",AuthVerifyMiddleware,ProfileController.UpdateProfile);
+
+
+router.post("/CreateToDo",AuthVerifyMiddleware,ToDoListController.CreateToDo);
+router.get("/SelectToDo",AuthVerifyMiddleware,ToDoListController.SelectToDo);
+router.post("/UpdateToDo",AuthVerifyMiddleware,ToDoListController.UpdateToDo);
+router.post("/UpdateStatusToDo",AuthVerifyMiddleware,ToDoListController.UpdateStatusToDo);
 router.post("/RemoveToDo",AuthVerifyMiddleware,ToDoListController.RemoveToDo);
+router.post("/SelectToDoByStatus",AuthVerifyMiddleware,ToDoListController.SelectToDoByStatus);
+router.post("/SelectToDoByDate",AuthVerifyMiddleware,ToDoListController.SelectToDoByDate);
 
 
-module.exports = router;
+module.exports=router;
